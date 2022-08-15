2b618b0898fac73f477d7688d6eec8a7bd578fe5
waldemar-sarmiento <waldemar-sarmiento>
2022-08-15 00:28:15 +0000

    adding None

--- Example/Ecample.py
+++ Example/Ecample.py
@@ -1,3 +1,15 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC select * from diamonds
+
+# COMMAND ----------
+
+my_name = None
+
+# COMMAND ----------
+
+df = spark.range(16)
+
+# COMMAND ----------
+
+
